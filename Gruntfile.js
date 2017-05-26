5ce4b775195d70c402cb0cacd96429d2829b3777
Joshua Fehler <jsfehler@gmail.com>
2017-05-26 10:07:35 -0400

    Rename doc directory to docs

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,7 +23,7 @@ module.exports = function (grunt) {
 			dist: {
 				src: ['src/**/*.js'],
 				options: {
-					destination: 'doc',
+					destination: 'docs',
 					template : "node_modules/ink-docstrap/template",
               		configure : "node_modules/ink-docstrap/template/jsdoc.conf.json"
 				}
