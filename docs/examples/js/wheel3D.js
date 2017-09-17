3ea33a4bdfa05562700cd8a69eb3d463579c008f
Joshua Fehler <jsfehler@gmail.com>
2017-09-16 20:09:03 -0400

    Update Wheel3D example

--- docs/examples/js/wheel3D.js
+++ docs/examples/js/wheel3D.js
@@ -1,7 +1,11 @@
+var menuWheel;
+
 var game = new Phaser.Game(600, 400, Phaser.AUTO, 'wheel3D', { preload: preload, create: create });
 
 function preload() {
 	game.load.image("icon", "assets/wheel3D/icon.png");
+    game.load.image("mmUp", "assets/wheel3D/mmUp.png");
+    game.load.image("mmDown", "assets/wheel3D/mmDown.png");
 }
 
 function create() {
@@ -15,7 +19,7 @@ function create() {
     }
 
     // Create the wheel.
-    var menuWheel = new uiWidgets.Wheel3D(
+    menuWheel = new uiWidgets.Wheel3D(
         game,
         {"x": game.world.centerX, "y": game.world.centerY},
         menuList,
@@ -27,4 +31,30 @@ function create() {
 
     menuWheel.activate();
 
+    // Scroll Buttons
+    var scrollLeftButton = this.game.add.button(
+        game.width-74,
+        game.world.centerY - 26,
+        "mmUp",
+        scrollTheWheel,
+        this);
+    scrollLeftButton.dir = 0;
+
+    var scrollRightButton = this.game.add.button(
+        game.width-74,
+        game.world.centerY + 26,
+        "mmDown",
+        scrollTheWheel,
+        this);
+    scrollRightButton.dir = 1;
+
 }
+
+var scrollTheWheel = function (item) {
+    if (item.dir === 0){
+        menuWheel.moveBack();
+    }
+    if (item.dir === 1){
+        menuWheel.moveForward();
+    }
+};
