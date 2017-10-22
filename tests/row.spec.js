c0b4b8ed8f956f2efeb7c90ef683429d3c8d5fa1
Joshua Fehler <jsfehler@gmail.com>
2017-10-21 21:14:42 -0400

    Update unit tests for row and column with padding tests

--- tests/row.spec.js
+++ tests/row.spec.js
@@ -13,6 +13,18 @@ describe('Row', function(){
         });
     });
 
+    describe('When I add a second node to a Row with padding', function(){
+        it('Then should be placed next to the first node, offset by the padding', function(){
+            var dummySprite = game.add.sprite(0, 0, 'dummySprite');
+            var dummySprite2 = game.add.sprite(0, 0, 'dummySprite');
+            row = new uiWidgets.Row(game);
+            row.addNode(dummySprite);
+            row.addNode(dummySprite2, null, 17);
+
+            chai.expect(row.children[1].x).to.equal(317);
+        });
+    });
+
     afterEach(function() {
         row.destroy();
     });
