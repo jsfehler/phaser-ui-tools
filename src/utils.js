31b3ed975c046e3163d290e765ab7b12fed25d59
Joshua Fehler <jsfehler@gmail.com>
2017-11-05 14:57:38 -0500

    Move wheel3D utils into separate file

---
+++ src/utils.js
@@ -0,0 +1,20 @@
+var uiWidgets = uiWidgets || {};
+
+uiWidgets.utils = {};
+
+/**
+ * A modulo operator that doesn't allow negative numbers.
+ * @param divdend
+ * @param divisor
+ */
+uiWidgets.utils.modulo = function(dividend, divisor) {
+    "use strict";
+    return ((((dividend) % divisor) + divisor) % divisor);
+};
+
+/**
+ * Select an operator action using a string value */
+uiWidgets.utils.operators = {
+    '+': function (a, b) { return a + b; },
+	'-': function (a, b) { return a - b; }
+};
