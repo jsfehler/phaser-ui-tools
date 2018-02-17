9952a0de41113b31676746b1f51ab91c18571597
Joshua Fehler <jsfehler@gmail.com>
2018-02-17 13:59:58 -0500

    Use double quotes in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,17 @@
 module.exports = function (grunt) {
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+        pkg: grunt.file.readJSON("package.json"),
         concat: {
-            options: {separator: ';'},
+            options: {separator: ";"},
             dist: {
-                src: ['src/utils.js', 'src/containers/frame.js', 'src/**/*.js'],
-                dest: 'dist/<%= pkg.name %>.js'
+                src: ["src/utils.js", "src/containers/frame.js", "src/**/*.js"],
+                dest: "dist/<%= pkg.name %>.js"
             }
         },
         copy: {
             main: {
-                src: 'dist/phaser-ui-tools.js',
-                dest: 'docs/examples/lib/phaser-ui-tools.js'
+                src: "dist/phaser-ui-tools.js",
+                dest: "docs/examples/lib/phaser-ui-tools.js"
             }
         },
         uglify: {
@@ -19,17 +19,17 @@ module.exports = function (grunt) {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
             },
             dist: {
-                files: {'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']}
+                files: {"dist/<%= pkg.name %>.min.js": ["<%= concat.dist.dest %>"]}
             }
         },
         jshint: {
-            files: ['Gruntfile.js', 'src/**/*.js', 'tests/*.js']
+            files: ["Gruntfile.js", "src/**/*.js", "tests/*.js"]
         },
         jsdoc: {
             dist: {
-                src: ['src/**/*.js'],
+                src: ["src/**/*.js"],
                 options: {
-                    destination: 'docs',
+                    destination: "docs",
                     template : "node_modules/ink-docstrap/template",
                     configure : "node_modules/ink-docstrap/template/jsdoc.conf.json"
                 }
@@ -37,7 +37,7 @@ module.exports = function (grunt) {
         },
         mocha: {
             all: {
-                src: ['tests/testrunner.html']
+                src: ["tests/testrunner.html"]
             },
             options: {
                 run: false,
@@ -50,13 +50,13 @@ module.exports = function (grunt) {
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-jsdoc');
-    grunt.loadNpmTasks('grunt-mocha');
+    grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-jsdoc");
+    grunt.loadNpmTasks("grunt-mocha");
 
-    grunt.registerTask('default', ['jshint', 'mocha', 'concat', 'uglify', 'jsdoc', 'copy']);
-    grunt.registerTask('travis', ['jshint', 'mocha']);
+    grunt.registerTask("default", ["jshint", "mocha", "concat", "uglify", "jsdoc", "copy"]);
+    grunt.registerTask("travis", ["jshint", "mocha"]);
 };
