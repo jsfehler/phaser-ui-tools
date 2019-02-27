ffa7d8aa283033a386b1524838efad59365c238f
Joshua Fehler <jsfehler@gmail.com>
2019-02-26 23:36:57 -0500

    Don't use Phaser align consts

---
+++ src/const.js
@@ -0,0 +1,16 @@
+// Matches the alignment consts from Phaser
+export const alignments = {
+    TOP_LEFT: 0,
+    TOP_CENTER: 1,
+    TOP_RIGHT: 2,
+    LEFT_TOP: 3,
+    LEFT_CENTER: 4,
+    LEFT_BOTTOM: 5,
+    CENTER: 6,
+    RIGHT_TOP: 7,
+    RIGHT_CENTER: 8,
+    RIGHT_BOTTOM: 9,
+    BOTTOM_LEFT: 10,
+    BOTTOM_CENTER: 11,
+    BOTTOM_RIGHT: 12,
+};
