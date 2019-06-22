0d4bc7f354e1ab47d7b57747e65ccf4c15cd494f
Joshua Fehler <jsfehler@gmail.com>
2019-06-21 20:41:41 -0400

    Add sendToBack() and alignIn() to Phaser3 sprite

--- src/phaserObjects/sprite.js
+++ src/phaserObjects/sprite.js
@@ -1,7 +1,45 @@
 let exportObject;
 
 if (Phaser.Sprite === undefined) {
-    class Phaser3Sprite extends Phaser.GameObjects.Sprite {}
+    class Phaser3Sprite extends Phaser.GameObjects.Sprite {
+        constructor(game, x, y, image) {
+            super(game, x, y, image);
+
+            // Map alignment constants to Phaser 3 Align functions
+            const alignIn = Phaser.Display.Align.In;
+            this.alignInMapping = {
+                0: alignIn.TopLeft,
+                1: alignIn.TopCenter,
+                2: alignIn.TopRight,
+                3: alignIn.LeftTop,
+                4: alignIn.LeftCenter,
+                5: alignIn.LeftBottom,
+                6: alignIn.Center,
+                7: alignIn.RightTop,
+                8: alignIn.RightCenter,
+                9: alignIn.RightBottom,
+                10: alignIn.BottomLeft,
+                11: alignIn.BottomCenter,
+                12: alignIn.BottomRight,
+            };
+        }
+
+        /** Immitate the API of PhaserCE.
+        * Move this object behind all others.
+        * @private
+        */
+        sendToBack() {
+            this.depth = -9999;
+        }
+
+        /** Immitate the API of PhaserCE.
+        * Align this object in another.
+        * @private
+        */
+        alignIn(other, align) {
+            this.alignInMapping[align](this, other, other.width);
+        }
+    }
 
     exportObject = Phaser3Sprite;
 } else {
