ecb086fec8226ff974abe983296878986a47cdae
Joshua Fehler <jsfehler@gmail.com>
2019-06-23 13:28:03 -0400

    Reorganize examples directory

---
+++ docs/examples/js/phaser3/hscrollbar.js
@@ -0,0 +1,62 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 400,
+    height: 300,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image("dummyButton", "assets/horizontal/sprite.png");
+    this.load.image("track", "assets/horizontal/track.png");
+    this.load.spritesheet('bar', 'assets/horizontal/bar.png', { frameWidth: 44, frameHeight: 22 });
+}
+
+function create() {
+    var viewport = new uiWidgets.Viewport(this, 75, 75, 260, 128);
+    var row = new uiWidgets.Row(this, 0, 0);
+
+    viewport.addNode(row);
+
+
+    // Add things to the row.
+    var dummy_sprite_a = this.add.image(0, 0, "dummyButton");
+    var dummy_sprite_b = this.add.image(0, 0, "dummyButton");
+    var dummy_sprite_c = this.add.image(0, 0, "dummyButton");
+    var dummy_sprite_d = this.add.image(0, 0, "dummyButton");
+    var dummy_sprite_e = this.add.image(0, 0, "dummyButton");
+    var dummy_sprite_f = this.add.image(0, 0, "dummyButton");
+    var dummy_sprite_g = this.add.image(0, 0, "dummyButton");
+    var dummy_sprite_h = this.add.image(0, 0, "dummyButton");
+    var dummy_sprite_i = this.add.image(0, 0, "dummyButton");
+
+    row.addNode(dummy_sprite_a);
+    row.addNode(dummy_sprite_b);
+    row.addNode(dummy_sprite_c);
+    row.addNode(dummy_sprite_d);
+    row.addNode(dummy_sprite_e);
+    row.addNode(dummy_sprite_f);
+    row.addNode(dummy_sprite_g);
+    row.addNode(dummy_sprite_h);
+    row.addNode(dummy_sprite_i);
+
+    var scrollbar = new uiWidgets.Scrollbar(
+        this,
+        viewport,
+        true,
+        false,
+        "track",
+        "bar",
+        {'duration': 300, 'ease': Phaser.Math.Easing.Quadratic.Out}
+    );
+
+    Phaser.Display.Align.To.BottomCenter(scrollbar, viewport, 0, 128 + 10);
+
+}
+
+function update() {}
---
+++ docs/examples/js/phaser3/valuebar.js
@@ -0,0 +1,108 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 400,
+    height: 300,
+    scene: {
+        preload: preload,
+        create: create,
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image("track", "assets/valuebar/track.png");
+    this.load.spritesheet('bar', 'assets/valuebar/bar.png', { frameWidth: 32, frameHeight: 32 });
+    this.load.image("vtrack", "assets/valuebar/vtrack.png");
+    this.load.image("alphaImage", "assets/valuebar/alpha.png");
+}
+
+var valuebar0;
+var valuebar50;
+var vvaluebar50;
+var valuebar0_text;
+var valuebar50_text;
+var vvaluebar50_text;
+var alphaImage;
+
+function create() {
+
+    // Create a valuebar starting at 0.
+    valuebar0 = new uiWidgets.ValueBar(
+        this,
+        {"x": 50, "y": 10},
+        {"step": 1, "startValue": 0, maxValue: 100},
+        true,
+        false,
+        "track",
+        "bar",
+        {'duration': 100, 'ease': Phaser.Math.Easing.Quadratic.Out}
+    );
+
+    valuebar0_text = this.add.text(50, 50, valuebar0.valueRange.startValue, {
+        font: "65px Arial",
+        fill: "#ff0044",
+        align: "center"
+    });
+
+    valuebar0.valueDisplay = valuebar0_text;
+
+    // Use the onMovement signal to update the display of text when the bar is moved.
+    valuebar0.emitter.on('movement', updateValueDisplay);
+
+    // Create a valuebar starting at 50.
+    valuebar50 = new uiWidgets.ValueBar(
+        this,
+        {"x": 50, "y": 150},
+        {"step": 25, "startValue": 50, maxValue: 100},
+        true,
+        false,
+        "track",
+        "bar",
+        {'duration': 100, 'ease': Phaser.Math.Easing.Quadratic.Out}
+    );
+
+    valuebar50_text = this.add.text(50, 200, valuebar50.valueRange.startValue, {
+        font: "65px Arial",
+        fill: "#ff0044",
+        align: "center"
+    });
+
+    valuebar50.valueDisplay = valuebar50_text;
+    valuebar50.emitter.on('movement', updateValueDisplay);
+
+    // Create a vertical valuebar starting at 50.
+    vvaluebar50 = new uiWidgets.ValueBar(
+        this,
+        {"x": 350, "y": 50},
+        {"step": 25, "startValue": 50, maxValue: 100},
+        true,
+        true,
+        "vtrack",
+        "bar",
+        {'duration': 100, 'ease': Phaser.Math.Easing.Quadratic.Out}
+    );
+
+    vvaluebar50_text = this.add.text(400, 200, valuebar50.valueRange.startValue, {
+        font: "65px Arial",
+        fill: "#ff0044",
+        align: "center"
+    });
+
+    vvaluebar50.valueDisplay = vvaluebar50_text;
+    vvaluebar50.emitter.on('movement', updateValueDisplay);
+
+    alphaImage = this.add.sprite(500, 100, "alphaImage");
+    alphaImage.alpha = vvaluebar50.valueRange.getCurrentValue() / 100;
+
+    // Use the onMovement signal to updte the sprite's opacity whenever the bar is moved.
+    vvaluebar50.emitter.on('movement', updateAlpha);
+}
+
+function updateValueDisplay(bar) {
+    bar.valueDisplay.setText(bar.valueRange.getCurrentValue());
+}
+
+function updateAlpha(bar) {
+    alphaImage.alpha = bar.valueRange.getCurrentValue() / 100;
+}
